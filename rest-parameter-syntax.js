29d7b00a0fef6858fa285d63620516a23e2fda15
lukaszkukla <lukaszkukla@gmail.com>
2022-05-04 19:05:17 +0000

    Initial commit

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,43 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
+// Regular function call
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log('sum:', sum); 
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log('sum2:', sum2); 
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for(let i of rest) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log('sum3:', sum3);
+
+
+let arr3 = [4,5,6];
+let arr4 = [...arr3];
+
+console.log(arr3);
+console.log(arr4);
+
+// Rest operator challenge
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
+const updateSortReverse = (arr, ...letters) => [...arr, ...letters].sort().reverse();
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
+
+console.log(reverseSort);
+console.log(mixedLetters);
+
+console.log(moreMixedLetters);
